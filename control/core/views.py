fdba84bc6bd6ea21ea1e762b8a9a97b8e6e84fdf
Felix Vera <verafelixbaetcke03@gmail.com>
2024-08-28 12:05:36 -0400

    vistas con el crud ya creado.

--- control/core/views.py
+++ control/core/views.py
@@ -1,3 +1,48 @@
-from django.shortcuts import render
+from django.shortcuts import render, rever
+from django.views.generic import ListView, CreateView, DetailView, UpdateView, DeleteView, TemplateView
+from core.models import Task
+from django.urls import reverse_lazy
+from core.forms import TaskForm
 
-# Create your views here.
+
+class HomeView(TemplateView):
+    template_name= 'core/home.html'
+
+
+#lista
+class TaskListView(ListView):
+    model = Task
+    paginate_by = 10
+    template_name = 'core/task_list.html'
+    content_object_name = 'tasks'
+
+
+#crear
+class TaskFormMixin(CreateView,UpdateView):
+    model = Task
+    template_name = 'core/task_create.html'
+    form_class = TaskForm
+    success_url = reverse_lazy('core:task_list')
+
+
+class TaskCreateView(CreateView):
+    pass
+
+
+class TaskUpdateView(UpdateView):
+    pass
+
+
+
+class TaskDeleteView(DeleteView):
+    model = Task
+    template_name= 'core/task_delete.html'
+    success_url = reverse_lazy('core:task_list')
+
+#detail
+class TaskDetailView(DetailView):
+    model = Task
+    template_name = 'core/task_detail.html'
+    context_object_name = 'task'
+
+
