a7d8520d3ddde2b6e0c17a8dd52249e246b78c6e
Felix Vera <verafelixbaetcke03@gmail.com>
2024-08-29 14:55:17 -0400

    pandas y demas cambios xD

--- control/core/urls.py
+++ control/core/urls.py
@@ -8,8 +8,8 @@ app_name  = 'core'
 urlpatterns = [
     path('',views.HomeView.as_view(), name='home'),
     path('tasks/', views.TaskListView.as_view(), name='task_list'),
+    path('task/toggle/<int:task_id>/', views.toggle_task, name='toggle_task'),
     path('tasks/create/', views.TaskCreateView.as_view(), name='task_create'),
     path('tasks/<int:pk>/update/', views.TaskUpdateView.as_view(), name='task_update'),
     path('tasks/<int:pk>/delete/', views.TaskDeleteView.as_view(), name='task_delete'),
-    
 ]
--- control/core/views.py
+++ control/core/views.py
@@ -1,10 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_list_or_404
 from django.views.generic import ListView,DetailView, CreateView, UpdateView, DeleteView, TemplateView
 from core.models import Task
 from django.urls import reverse_lazy
 from core.forms import TaskForm
 from django.contrib import messages
-
+import pandas as pd
 
 
 class HomeView(TemplateView):
@@ -16,6 +16,17 @@ class TaskListView(ListView):
     paginate_by = 10
     template_name = 'core/task_list.html'
     context_object_name = 'tasks'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        tasks = context['tasks']
+
+        df = pd.DataFrame()
+
+
+
+
 
 # Crear y actualizar
 class TaskCreateView(CreateView):
@@ -42,3 +53,14 @@ class TaskDeleteView(DeleteView):
     model = Task
     template_name = 'core/task_delete.html'
     success_url = reverse_lazy('core:task_list')
+
+
+
+def toggle_task(request, task_id):
+    task  = get_list_or_404(Task, id = task_id)
+    task.estado = not task.estado
+    task.save()
+    return redirect('core:task_list')
+
+
+
